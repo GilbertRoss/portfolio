02a0cd40ffaf8cfa6acb25fffd98f7ebf3b55cc3
GilbertRoss <>
2020-09-25 22:20:30 +0200

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./07.png"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <div class="flex flex-row  mt-4 menu-container">
+          <div class="flex flex-row ">
+            <div class="text-gray-700 text-center px-4 py-2 m-2">
+              sb.
+          </div>
+          </div>
+          <div class="flex flex-row text-gray-700 float-left menu-bar">
+            <div class="text-gray-700 text-center px-4 py-2 m-2 font-color-menu">Curriculum</div>
+            <div class="text-gray-700 text-center px-4 py-2 m-2 font-color-menu">Projects</div>
+            <div class="text-gray-700 text-center px-4 py-2 m-2 font-color-menu">Contact me</div>
+          </div>
+        </div>
+        </div>
+        <section>
+        <div className="flex flex-row">
+          <div className="title">Hello, my name is Sadman! I am Software developer based in Bolzano.</div>
+          <div class="title-img"><img src={require('./07.png')} /></div>
+        </div>
+        </section>
     </div>
   );
 }
